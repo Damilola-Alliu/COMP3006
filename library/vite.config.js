ae1dfb5c3ef8db164d58387d21bf7f63400bb76e
Damilola-Alliu <muhammed-ali.alliu@students.plymouth.ac.uk>
2023-12-31 13:19:37 +0000

    resolving gh build

--- library/vite.config.js
+++ library/vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'build', // This line sets the output directory to 'build'
+  },
   test: {
     globals: true,
     environment: "jsdom",
